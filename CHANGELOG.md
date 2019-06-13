2af791a2b7343a34e1cfda7096266c6e1c398b6d
DmitryTsepelev <dmitry.a.tsepelev@gmail.com>
2019-06-13 19:47:26 +0300

    Properly validate arrays

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,9 +2,11 @@
 
 ## master
 
+- [PR #12](https://github.com/DmitryTsepelev/store_model/pull/12) Fixes [Issue #11](https://github.com/DmitryTsepelev/store_model/pull/11)
+
 ## 0.3.1 (2019-06-13)
 
-- [PR #10](https://github.com/DmitryTsepelev/store_model/pull/6) Fixes [Issue #9](https://github.com/DmitryTsepelev/store_model/pull/9)
+- [PR #10](https://github.com/DmitryTsepelev/store_model/pull/10) Fixes [Issue #9](https://github.com/DmitryTsepelev/store_model/pull/9)
 
 ## 0.3.0 (2019-05-06)
 
--- spec/active_model/validations/store_model_validator_spec.rb
+++ spec/active_model/validations/store_model_validator_spec.rb
@@ -4,7 +4,7 @@ require "spec_helper"
 
 RSpec.describe ActiveModel::Validations::StoreModelValidator do
   shared_examples "check store model is invalid" do
-    it "is invalid because configuration is invalid" do
+    it "returns correct error messages" do
       expect(subject.errors.messages).to eq(configuration: ["is invalid"])
       expect(subject.errors.full_messages).to eq(["Configuration is invalid"])
 
@@ -21,50 +21,111 @@ RSpec.describe ActiveModel::Validations::StoreModelValidator do
     product
   end
 
-  context "with store_model validator" do
-    let(:custom_product_class) do
-      build_custom_product_class do
-        attribute :configuration, Configuration.to_type
-        validates :configuration, store_model: true
+  context "with a singular type" do
+    context "with store_model validator" do
+      let(:custom_product_class) do
+        build_custom_product_class do
+          attribute :configuration, Configuration.to_type
+          validates :configuration, store_model: true
+        end
+      end
+
+      context "when store_model value is not nil" do
+        it { is_expected.to be_invalid }
+
+        include_examples "check store model is invalid"
+      end
+
+      context "when store_model value is nil" do
+        let(:attributes) { { configuration: nil } }
+
+        it { is_expected.to be_invalid }
+
+        it "is invalid because configuration is blank" do
+          expect(subject.errors.messages).to eq(configuration: ["can't be blank"])
+          expect(subject.errors.full_messages).to eq(["Configuration can't be blank"])
+        end
       end
     end
 
-    context "when store_model value is not nil" do
-      it { is_expected.to be_invalid }
+    context "with allow_nil: true" do
+      let(:custom_product_class) do
+        build_custom_product_class do
+          attribute :configuration, Configuration.to_type
+          validates :configuration, allow_nil: true, store_model: true
+        end
+      end
 
-      include_examples "check store model is invalid"
-    end
+      context "when store_model value is not nil" do
+        it { is_expected.to be_invalid }
 
-    context "when store_model value is nil" do
-      let(:attributes) { { configuration: nil } }
+        include_examples "check store model is invalid"
+      end
 
-      it { is_expected.to be_invalid }
+      context "when store_model value is nil" do
+        let(:attributes) { { configuration: nil } }
 
-      it "is invalid because configuration is blank" do
-        expect(subject.errors.messages).to eq(configuration: ["can't be blank"])
-        expect(subject.errors.full_messages).to eq(["Configuration can't be blank"])
+        it { is_expected.to be_valid }
       end
     end
   end
 
-  context "with allow_nil: true" do
-    let(:custom_product_class) do
-      build_custom_product_class do
-        attribute :configuration, Configuration.to_type
-        validates :configuration, allow_nil: true, store_model: true
+  context "with an array type" do
+    context "with store_model validator" do
+      let(:custom_product_class) do
+        build_custom_product_class do
+          attribute :configuration, Configuration.to_array_type
+          validates :configuration, store_model: true
+        end
+      end
+
+      context "when array is empty" do
+        it { is_expected.to be_valid }
+      end
+
+      context "when array member is invalid" do
+        let(:attributes) { { configuration: [Configuration.new] } }
+
+        it { is_expected.to be_invalid }
+
+        it "returns correct error messages" do
+          expect(subject.errors.messages).to eq(configuration: ["is invalid"])
+          expect(subject.errors.full_messages).to eq(["Configuration is invalid"])
+
+          expect(subject.configuration.first.errors.messages).to eq(color: ["can't be blank"])
+          expect(subject.configuration.first.errors.full_messages).to eq(["Color can't be blank"])
+        end
+      end
+
+      context "when store_model value is nil" do
+        let(:attributes) { { configuration: nil } }
+
+        it { is_expected.to be_invalid }
+
+        it "is invalid because configuration is blank" do
+          expect(subject.errors.messages).to eq(configuration: ["can't be blank"])
+          expect(subject.errors.full_messages).to eq(["Configuration can't be blank"])
+        end
       end
     end
 
-    context "when store_model value is not nil" do
-      it { is_expected.to be_invalid }
+    context "with allow_nil: true" do
+      let(:custom_product_class) do
+        build_custom_product_class do
+          attribute :configuration, Configuration.to_array_type
+          validates :configuration, allow_nil: true, store_model: true
+        end
+      end
 
-      include_examples "check store model is invalid"
-    end
+      context "when array is empty" do
+        it { is_expected.to be_valid }
+      end
 
-    context "when store_model value is nil" do
-      let(:attributes) { { configuration: nil } }
+      context "when store_model value is nil" do
+        let(:attributes) { { configuration: nil } }
 
-      it { is_expected.to be_valid }
+        it { is_expected.to be_valid }
+      end
     end
   end
 end
