8408885e2a14bfa759e88b3bf10c6e923fc5f16a
Dmitry Tsepelev <dmitry.a.tsepelev@gmail.com>
2019-07-31 00:17:11 +0300

    Propery handle nested collections (#21)

--- lib/store_model/nested_attributes.rb
+++ lib/store_model/nested_attributes.rb
@@ -2,10 +2,30 @@
 
 module StoreModel
   module NestedAttributes
-    def accepts_nested_attributes_for(*associations)
-      associations.each do |association|
-        alias_method "#{association}_attributes=", "#{association}="
+    def self.included(base)
+      base.extend ClassMethods
+    end
+
+    module ClassMethods
+      def accepts_nested_attributes_for(*associations)
+        associations.each do |association|
+          case attribute_types[association.to_s]
+          when Types::JsonType
+            alias_method "#{association}_attributes=", "#{association}="
+          when Types::ArrayType
+            define_method "#{association}_attributes=" do |attributes|
+              assign_nested_attributes_for_collection_association(association, attributes)
+            end
+          end
+        end
       end
+    end
+
+    private
+
+    def assign_nested_attributes_for_collection_association(association, attributes)
+      attributes = attributes.values if attributes.is_a?(Hash)
+      send "#{association}=", attributes
     end
   end
 end
--- spec/store_model/nested_attributes_spec.rb
+++ spec/store_model/nested_attributes_spec.rb
@@ -22,11 +22,11 @@ RSpec.describe StoreModel::NestedAttributes do
 
   describe "#initialize" do
     context "when symbolized hash is passed" do
-      it("assigns attrbutes to root model") do
+      it("assigns attributes to root model") do
         expect(subject).to have_attributes(attributes.slice(:color))
       end
 
-      it("assigns attrbutes to nested model") do
+      it("assigns attributes to nested model") do
         expect(subject.suppliers.first).to have_attributes(supplier1)
         expect(subject.suppliers.second).to have_attributes(supplier2)
       end
@@ -40,16 +40,24 @@ RSpec.describe StoreModel::NestedAttributes do
   end
 
   describe "#accepts_nested_attributes_for" do
+    let(:suppliers_attributes) { [supplier1, supplier2] }
+
     let(:attributes) do
-      {
-        color: "red",
-        suppliers_attributes: [supplier1, supplier2]
-      }
+      { color: "red", suppliers_attributes: suppliers_attributes }
     end
 
-    it("assigns attrbutes to nested model") do
+    it "assigns attributes to nested model" do
       expect(subject.suppliers.first).to have_attributes(supplier1)
       expect(subject.suppliers.second).to have_attributes(supplier2)
+    end
+
+    context "when attributes are passed as a hash instead of array of hashes" do
+      let(:suppliers_attributes) { { "0" => supplier1, "1" => supplier2 } }
+
+      it "assigns attributes to nested model" do
+        expect(subject.suppliers.first).to have_attributes(supplier1)
+        expect(subject.suppliers.second).to have_attributes(supplier2)
+      end
     end
 
     context "when association is singular" do
@@ -66,13 +74,51 @@ RSpec.describe StoreModel::NestedAttributes do
 
       let(:attributes) { { color: "red", supplier_attributes: supplier1 } }
 
-      it("assigns attrbutes to root model") do
+      it("assigns attributes to root model") do
         expect(subject).to have_attributes(attributes.slice(:color))
       end
 
-      it("assigns attrbutes to nested model") do
+      it("assigns attributes to nested model") do
         expect(subject.supplier).to have_attributes(supplier1)
       end
+    end
+  end
+
+  describe "#valid?" do
+    let(:child_class) do
+      Class.new do
+        def self.model_name
+          ActiveModel::Name.new(self, nil, "FormInput")
+        end
+
+        include StoreModel::Model
+
+        attribute :data, :string
+
+        validates :data, presence: true
+      end
+    end
+
+    let(:parent_class) do
+      children_type = child_class.to_array_type
+
+      Class.new do
+        def self.model_name
+          ActiveModel::Name.new(self, nil, "Form")
+        end
+
+        include StoreModel::Model
+
+        attribute :children, children_type
+
+        validates :children, store_model: true
+      end
+    end
+
+    context "when child model is invalid" do
+      subject { parent_class.new(children: [child_class.new]) }
+
+      it { is_expected.to be_invalid }
     end
   end
 end
