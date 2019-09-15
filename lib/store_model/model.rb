71c31743c634516a7f5553527fe35001d28aa8ab
Zoran Pesic <zoran1991@gmail.com>
2019-09-15 16:21:03 -0700

    address rubocop violation

--- lib/store_model/model.rb
+++ lib/store_model/model.rb
@@ -49,7 +49,8 @@ module StoreModel
     #
     # @return [String]
     def inspect
-      attribute_string = attributes.map { |name, value| "#{name}: #{value.nil? ? 'nil' : value}" }.join(", ")
+      attribute_string = attributes.map { |name, value| "#{name}: #{value.nil? ? 'nil' : value}" }
+                                   .join(", ")
       "#<#{self.class.name} #{attribute_string}>"
     end
 
