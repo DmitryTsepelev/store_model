aeea4f47657a1713a271d9da3dbd0355c3612a89
Fabio Gomes <fabioxgn@gmail.com>
2022-06-27 11:15:10 -0300

    Fixes #121

--- lib/store_model/ext/parent_assignment.rb
+++ lib/store_model/ext/parent_assignment.rb
@@ -9,7 +9,9 @@ module StoreModel
       assign_parent_to_singular_store_model(attribute)
       return unless attribute.is_a?(Array)
 
-      attribute.each(&method(:assign_parent_to_singular_store_model))
+      # Do not use &method here or it will break if the model has a method attribute
+      # See https://github.com/DmitryTsepelev/store_model/issues/121
+      attribute.each { |a| assign_parent_to_singular_store_model(a) }
     end
 
     def assign_parent_to_singular_store_model(item)
--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -3,7 +3,7 @@
 ActiveRecord::Schema.define(version: 2019_02_216_153105) do
   create_table :products do |t|
     t.string :name
-    t.string :method
+    t.string :method # Reproduces #121: https://github.com/DmitryTsepelev/store_model/issues/121
     t.json :configuration, default: {}
   end
 end
