186d4db669f833988ce95b6bc3eeb4d9f060b309
Robert Pankowecki <robert.pankowecki@toptal.com>
2024-04-03 15:56:31 +0200

    disable parent tracking

--- lib/store_model/railtie.rb
+++ lib/store_model/railtie.rb
@@ -6,10 +6,14 @@ require "store_model/ext/active_record/base"
 module StoreModel # :nodoc:
   class Railtie < Rails::Railtie # :nodoc:
     config.to_prepare do |_app|
-      ActiveSupport.on_load(:active_record) do
-        ActiveModel::Attributes.prepend(Attributes)
-        prepend(Base)
-      end
+      # Disable parent tracking functionality
+      # which patches ActiveRecord and ActiveModel
+      # functionality. 
+      #
+      # ActiveSupport.on_load(:active_record) do
+      #   ActiveModel::Attributes.prepend(Attributes)
+      #   prepend(Base)
+      # end
     end
   end
 end
